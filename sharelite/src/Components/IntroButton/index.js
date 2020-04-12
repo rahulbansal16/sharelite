7f2e930647988e19f66dad0480f1f0395b859dcb
Rahul Bansal <bansal.rahul14@gmail.com>
2020-04-12 17:27:02 +0530

    created the feed page

---
+++ sharelite/src/Components/IntroButton/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Button} from 'semantic-ui-react';
+
+// Add your post over here
+// This will be an amazing thing to do
+
+export class IntroButton extends React.Component{
+
+    render(){
+        return(
+            <Button text>
+                Create Post
+            </Button>
+        )
+    }
+
+}
---
+++ sharelite/src/Containers/FeedPage/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {Grid}  from 'semantic-ui-react';
+import {Preview} from './../../Components/Preview';
+import {Intro} from './../../Components/Intro';
+import { IntroButton } from '../../Components/IntroButton';
+
+
+const introJson = [
+        {
+            heading:'Marketing',
+            exp:'< 1',
+            college:'IIM Rohtak',
+            tags:['finance', 'marketing','strategy','MS Excel', 'MS Word', 'C++', 'Java', 'Salesforce', 'GitHub', 'PowerPoint'],
+            question: 'I am Shini Jain, first year MBA student at Indian Institute of Management, Rohtak. I am a computer engineer and have work experience in Accenture for 23 months in technology. Due to the ongoing COVID-19 pandemic, the internship I was offered stands cancelled. I am actively looking for internship opportunities in varied domains and my areas of interest are Marketing, Research, Strategy, Consulting and Finance. Pursuing summer internship from your organization will enrich my learning and give me the necessary exposure. The duration Iamm looking for the internship is 6-8 weeks i.e., from April to May.' ,
+            email:'pgp10shinij@iimrohtak.ac.in' ,
+            linkedin:'https://www.linkedin.com/in/shinijain/',
+        },
+        {
+            heading:'Marketing',
+            exp:'< 1',
+            college:'IIM Rohtak',
+            tags:['finance', 'marketing','strategy','MS Excel', 'MS Word', 'C++', 'Java', 'Salesforce', 'GitHub', 'PowerPoint'],
+            question: 'I am Shini Jain, first year MBA student at Indian Institute of Management, Rohtak. I am a computer engineer and have work experience in Accenture for 23 months in technology. Due to the ongoing COVID-19 pandemic, the internship I was offered stands cancelled. I am actively looking for internship opportunities in varied domains and my areas of interest are Marketing, Research, Strategy, Consulting and Finance. Pursuing summer internship from your organization will enrich my learning and give me the necessary exposure. The duration Iamm looking for the internship is 6-8 weeks i.e., from April to May.' ,
+            email:'pgp10shinij@iimrohtak.ac.in' ,
+            linkedin:'https://www.linkedin.com/in/shinijain/',
+        },
+        {
+            heading:'Marketing',
+            exp:'< 1',
+            college:'IIM Rohtak',
+            tags:['finance', 'marketing','strategy','MS Excel', 'MS Word', 'C++', 'Java', 'Salesforce', 'GitHub', 'PowerPoint'],
+            question: 'I am Shini Jain, first year MBA student at Indian Institute of Management, Rohtak. I am a computer engineer and have work experience in Accenture for 23 months in technology. Due to the ongoing COVID-19 pandemic, the internship I was offered stands cancelled. I am actively looking for internship opportunities in varied domains and my areas of interest are Marketing, Research, Strategy, Consulting and Finance. Pursuing summer internship from your organization will enrich my learning and give me the necessary exposure. The duration Iamm looking for the internship is 6-8 weeks i.e., from April to May.' ,
+            email:'pgp10shinij@iimrohtak.ac.in' ,
+            linkedin:'https://www.linkedin.com/in/shinijain/',            
+        },
+];
+export class FeedPage extends React.Component {
+
+    render(){
+        return (
+            <>
+                <Grid columns={2}
+                      divided
+                      style= {{margin:'10px'}}
+                      stackable>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <div style = {{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', height:'100%vh', marginTop:'10%'}}>
+                                <Preview style={{display:'inline'}}/>
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div style = {{alignItems:'rights'}}>
+                                <IntroButton style={{display:'inline'}}/>
+                                <Intro data={introJson} style={{display:'inline'}}/>
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </>
+        );
+    }
+}
+
+
