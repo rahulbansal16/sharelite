cd6da675dd7ac00bc4dec623e46ad58662d29206
Rahul Bansal <bansal.rahul14@gmail.com>
2020-04-12 21:39:11 +0530

    Added the video Embed code

--- sharelite/src/Components/Preview/index.js
+++ sharelite/src/Components/Preview/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Embed, Card, Header} from 'semantic-ui-react';
+import {Embed, Card,Image, Header} from 'semantic-ui-react';
 // import {code} from '../../images/code.jpg';
 export class Preview extends React.Component{
     constructor(props){
@@ -11,8 +11,18 @@ export class Preview extends React.Component{
         return (
             <>
             <Header as='h1' textAlign='center'>Live Feed</Header>
-            <Embed id = {this.props.id}>
-            </Embed>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/QnZHO7QvjaM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            {/* <Embed
+            id='QnZHO7QvjaM'
+            placeholder="https://i.ytimg.com/vi/QnZHO7QvjaM/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&amp;rs=AOn4CLDpYsMiOU_YFPy2oHNHmCx_BzpDFQ"
+            source='youtube'
+            >
+            </Embed> */}
+            {/* <Image
+              size="large"
+              src="https://i.ytimg.com/vi/QnZHO7QvjaM/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&amp;rs=AOn4CLDpYsMiOU_YFPy2oHNHmCx_BzpDFQ"
+              style={{ marginRight: "1.5em" }}
+            /> */}
             </>
         );
     }
